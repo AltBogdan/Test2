54fac370ae93ddb4cba59277c40dbfcb9af06d3b
AltBogdan <valuyshokbogdan@gmail.com>
2019-12-12 19:33:09 +0200

    TEST

---
+++ ЛР11/ЛР11.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "string"
+using namespace std;
+
+int main()
+{
+	string s, s0; char c = '\0';
+	std::cout << "s0 = "; getline(cin, s0);
+	std::cout << "s = ";  getline(cin, s);
+	std::cout << "c = ";  cin >> c;
+
+	string::size_type npos = 0;
+	while ((npos = s.find(c, ++npos)) != string::npos)
+	{
+		s.erase(npos, 1);
+		s.insert(npos, s0);
+	}
+
+	cout << "\nstring = " << s << endl;
+
+	std::cin.get();
+
+	return 0;
+
+}
+
